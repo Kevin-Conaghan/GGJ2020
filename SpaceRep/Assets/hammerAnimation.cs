5027db3eddf110a8cbf222e5ecb975b9e0366f3a
LewisFranklin1 <60544198+LewisFranklin1@users.noreply.github.com>
2020-02-02 10:47:20 +0000

    animation

---
+++ SpaceRep/Assets/hammerAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hammerAnimation : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayAnimation();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StopAnimation();
+        }
+    }
+    public void PlayAnimation()
+    {
+        if (null != anim)
+        {
+            // play Bounce but start at a quarter of the way though
+            anim.Play("HammerAnimation");
+            //anim.enabled = true;
+        }
+    }
+    public void StopAnimation()
+    {
+        if (null != anim)
+        {
+            // play Bounce but start at a quarter of the way though
+            anim.enabled = false;
+        }
+    }
+}
